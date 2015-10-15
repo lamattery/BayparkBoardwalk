5b899a26ab13085304abf417260b92b9d6eb386f
Jeff Lamattery <lamattery@gmail.com>
2015-10-15 09:59:05 -0700

    Green Buffers
    Added green buffers to either side of Morena

---
+++ Assets/KeyboardControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.P)) {
+            Application.CaptureScreenshot("luceCorner.png");
+        }
+	}
+}
